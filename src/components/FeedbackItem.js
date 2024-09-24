faa069bbdb554ce5afb5e78e8bbb016a3924779f
Gülcan Kamat <gulcankamat@gmail.com>
2024-09-24 16:33:45 +0200

    Adding work from feedback-app-frontend

---
+++ src/components/FeedbackItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FeedbackItem = ({feedback}) => {
+
+    return(
+        <div className="feedback-item">
+            <h3>{feedback.title}</h3>
+            <p>{feedback.text}</p>
+        </div>
+
+    )
+}
+
+export default FeedbackItem;
