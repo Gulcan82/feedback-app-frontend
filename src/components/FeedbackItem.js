9d16f8aac93fea78b0b2cc10ef5a103c8a261727
Gülcan Kamat <gulcankamat@gmail.com>
2024-09-25 10:45:37 +0200

    correcting the code.

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { deleteFeedback } from "../api/feedbackApi";
 
 const FeedbackItem = ({feedback}) => {
+    const handleDelete = async() => {
+        await deleteFeedback(feedback.title);
+        
+    }
 
     return(
         <div className="feedback-item">
             <h3>{feedback.title}</h3>
             <p>{feedback.text}</p>
+            <button onClick={{handleDelete}}>Delete</button>
         </div>
 
-    )
-}
+    );
+};
 
 export default FeedbackItem;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const BACKEND_HOST = process.env.FEEDBACK_BACKEND_HOST || "127.0.0.1";
-const BACKEND_PORT = process.env.FEEDBACK_BACKEND_PORT || 51815;
+const BACKEND_HOST = process.env.FEEDBACK_BACKEND_HOST || "192.168.73.146";
+const BACKEND_PORT = process.env.FEEDBACK_BACKEND_PORT || 31815;
 
 export const BACKEND_URL = `http://${BACKEND_HOST}:${BACKEND_PORT}/feedback`;
